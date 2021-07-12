bcd6661729afef29a516f62822a92985efe76e36
JoelCDL <joel.hagedorn@ucop.edu>
2021-07-12 16:54:25 -0700

    transform hero images to reduce file sizes

--- js/random-hero-image.js
+++ js/random-hero-image.js
@@ -1,12 +1,20 @@
 // Random Hero Image //
 
+// Transform source images to reduce file sizes via Parcel (see: https://v2.parceljs.org/recipes/image/):
+
+import heroImg1 from 'url:../images/hero/1-large.jpg?as=jpg&quality=30&width=1000'
+import heroImg2 from 'url:../images/hero/2-large.jpg?as=jpg&quality=30&width=1000'
+import heroImg3 from 'url:../images/hero/3-large.jpg?as=jpg&quality=30&width=1000'
+import heroImg4 from 'url:../images/hero/4-large.jpg?as=jpg&quality=30&width=1000'
+import heroImg5 from 'url:../images/hero/5-large.jpg?as=jpg&quality=30&width=1000'
+
 if (document.querySelector('.t-home')) {
   const imgArr = [
-    '1-large.jpg',
-    '2-large.jpg',
-    '3-large.jpg',
-    '4-large.jpg',
-    '5-large.jpg'
+    heroImg1,
+    heroImg2,
+    heroImg3,
+    heroImg4,
+    heroImg5
   ]
   const randomNum = Math.floor(Math.random() * imgArr.length)
   const heroImgEl = document.querySelector('.js-heroimage')
