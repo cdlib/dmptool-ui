9ab670bc1cfb25bf9f9ea3176723cfba4fa873a7
briri <brian.riley@ucop.edu>
2021-08-24 11:53:21 -0700

    fixed copy shell script so that it just copies hero images over with fingerprint

--- copy_assets_to_dmptool.sh
+++ copy_assets_to_dmptool.sh
@@ -6,11 +6,7 @@ npm run build
 # Copy all images over to the DMPTool assets directory
 cp dist/ui-assets/*.ico ../dmptool/app/assets/images
 cp dist/ui-assets/*.svg ../dmptool/app/assets/images
-cp dist/ui-assets/1-large.*.jpg ../dmptool/app/assets/images/hero/1-large.jpg
-cp dist/ui-assets/2-large.*.jpg ../dmptool/app/assets/images/hero/2-large.jpg
-cp dist/ui-assets/3-large.*.jpg ../dmptool/app/assets/images/hero/3-large.jpg
-cp dist/ui-assets/4-large.*.jpg ../dmptool/app/assets/images/hero/4-large.jpg
-cp dist/ui-assets/5-large.*.jpg ../dmptool/app/assets/images/hero/5-large.jpg
+cp dist/ui-assets/*.jpg ../dmptool/app/assets/images
 
 # Copy all fonts over to the DMPTool assets directory
 cp dist/ui-assets/SourceSans3VF-Italic.ttf.*.woff2 ../dmptool/app/assets/fonts/SourceSans3VF-Italic.ttf.woff2
