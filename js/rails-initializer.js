fac2699fc33c01773e31fa6b01ce8c52167094ab
JoelCDL <joel.hagedorn@ucop.edu>
2021-08-30 16:07:11 -0700

    refactor call-to-action and random-hero js as standalone comp

--- js/rails-initializer.js
+++ js/rails-initializer.js
@@ -4,18 +4,8 @@ import initProfileMenu from './profile-menu'
 import initLanguageMenu from './language-menu'
 import initAdminMenu from './admin-menu'
 
-import initCallToAction from './call-to-action'
 import initRandomHeroImage from './random-hero-image'
 import initBlogCarousel from './blog-carousel'
-
-// Import the hero images so that they get output in the dist/ui-assets.
-// The copy_assets_to_dmptool.sh then copies them into the Rails assets dir
-// so that they are available to the Rails HTML templates
-import heroImg1 from 'url:../images/hero/1-large.jpg?as=jpg&quality=30&width=1000'
-import heroImg2 from 'url:../images/hero/2-large.jpg?as=jpg&quality=30&width=1000'
-import heroImg3 from 'url:../images/hero/3-large.jpg?as=jpg&quality=30&width=1000'
-import heroImg4 from 'url:../images/hero/4-large.jpg?as=jpg&quality=30&width=1000'
-import heroImg5 from 'url:../images/hero/5-large.jpg?as=jpg&quality=30&width=1000'
 
 // Rails needs the JS to execute after page load once the DOM tree is available
 window.addEventListener("load", () => {
@@ -25,7 +15,6 @@ window.addEventListener("load", () => {
   initLanguageMenu()
   initAdminMenu()
 
-  initCallToAction()
   initRandomHeroImage()
   initBlogCarousel()
 });
