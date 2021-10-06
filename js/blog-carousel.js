cefb01936b550ec0e34a1be61583e4704d2b5dbf
JoelCDL <joel.hagedorn@ucop.edu>
2021-10-06 11:55:56 -0700

    add sample blog content, update blog js, fadein blog items

--- js/blog-carousel.js
+++ js/blog-carousel.js
@@ -1,29 +1,29 @@
 // Fetches the latest DMPTool blog content and scrolls through them
 
-// TODO: Convert this over to vanilla JS instead of JQuery!
-
 const initBlogCarousel = () => {
   // Rotate through the news items every 8 seconds
-  // #j-blog__content JSON is populated by the Rails application
-  const articles = $('#j-blog__content').val()
-  if (articles) {
+  // #j-blog__array JSON is populated by the Rails application
+
+  const articlesArray = document.querySelector('#j-blog__array')
+
+  if (articlesArray) {
+    const articles = articlesArray.value
     const news = JSON.parse(`${articles.replace(/\\"/g, '"').replace(/\\'/g, '\'')}`)
 
     const updateNews = (item) => {
-      const text = $('.c-blog__content');
-      text.hide();
-      text.html(`<a href="${news[item].link}" target="_blank" class="has-new-window-popup-info">${news[item].title}</a>`);
-      text.fadeIn(100);
-    };
+      const text = document.querySelector('.c-blog__content')
+      text.innerHTML = `<a href="${news[item].link}" target="_blank" class="has-new-window-popup-info">${news[item].title}</a>`
+    }
 
     const startNewsTimer = (item) => {
       setTimeout(() => {
-        updateNews(item);
-        startNewsTimer((item >= news.length - 1) ? 0 : item + 1);
-      }, 8000);
-    };
-    updateNews(0);
-    startNewsTimer(1);
+        updateNews(item)
+        startNewsTimer((item >= news.length - 1) ? 0 : item + 1)
+      }, 8000)
+    }
+
+    updateNews(0)
+    startNewsTimer(1)
   }
 }
 
