064d046326d99eb4248cfcefd7f302fbc5c30875
JoelCDL <joel.hagedorn@ucop.edu>
2021-10-26 15:46:52 -0700

    check that header nav and toggle js are not null

--- js/navtoggle.js
+++ js/navtoggle.js
@@ -1,31 +1,34 @@
 // Navigation Toggle Component //
 
-const navToggleButton = () => document.querySelector('#js-navtoggle')
-const headerNav = () => document.querySelector('#js-headernav')
+const navToggleButton = document.querySelector('#js-navtoggle')
+const headerNav = document.querySelector('#js-headernav')
+const inHeader = headerNav !== null && navToggleButton !== null
 
 const headerNavOpen = () => {
-  headerNav().hidden = false
-  navToggleButton().setAttribute('aria-expanded', true)
+  if (inHeader) {
+    headerNav.hidden = false
+    navToggleButton.setAttribute('aria-expanded', true)
+  }
 }
 
 const headerNavClosed = () => {
-  headerNav().hidden = true
-  navToggleButton().setAttribute('aria-expanded', false)
+  if (inHeader) {
+    headerNav.hidden = true
+    navToggleButton.setAttribute('aria-expanded', false)
+  }
 }
 
 const initNavToggle = () => {
-  const inHeader = document.querySelector('.c-header')
-
   if (inHeader) {
-    navToggleButton().addEventListener('click', () => {
-      if (headerNav().hidden === true) {
+    navToggleButton.addEventListener('click', () => {
+      if (headerNav.hidden === true) {
         headerNavOpen()
       } else {
         headerNavClosed()
       }
     })
 
-    if (headerNav().hidden === true) {
+    if (headerNav.hidden === true) {
       headerNavOpen()
     } else {
       headerNavClosed()
@@ -33,4 +36,4 @@ const initNavToggle = () => {
   }
 }
 
-export { headerNavOpen, headerNavClosed, initNavToggle}
+export { headerNavOpen, headerNavClosed, initNavToggle }
