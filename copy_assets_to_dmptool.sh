c5040cf45586e1fa2a6d50cffd4f8a962a00bc4d
briri <brian.riley@ucop.edu>
2021-11-16 10:10:38 -0800

    updated shell script to copy compiled assets to the Rails app

--- copy_assets_to_dmptool.sh
+++ copy_assets_to_dmptool.sh
@@ -3,10 +3,9 @@ npm install
 # Build the assets
 npm run build
 
-# Copy all images over to the DMPTool assets directory
-cp dist/ui-assets/*.ico ../dmptool/app/assets/images
-cp dist/ui-assets/*.svg ../dmptool/app/assets/images
-cp dist/ui-assets/*.jpg ../dmptool/app/assets/images
+# Copy over all assets to the Rails app
+mkdir -p ../dmptool/public/dmptool-ui
+cp -f dist/ui-assets/*.* ../dmptool/public/dmptool-ui/
 
 # Copy all fonts over to the DMPTool assets directory
 cp dist/ui-assets/SourceSans3VF-Italic.ttf.*.woff2 ../dmptool/app/assets/fonts/SourceSans3VF-Italic.ttf.woff2
@@ -14,8 +13,3 @@ cp dist/ui-assets/SourceSans3VF-Italic.ttf.*.woff ../dmptool/app/assets/fonts/So
 cp dist/ui-assets/SourceSans3VF-Roman.ttf.*.woff2 ../dmptool/app/assets/fonts/SourceSans3VF-Roman.ttf.woff2
 cp dist/ui-assets/SourceSans3VF-Roman.ttf.*.woff ../dmptool/app/assets/fonts/SourceSans3VF-Roman.ttf.woff
 
-# Copy all stylesheets over to the DMPTool assets directory
-cp dist/ui-assets/*.css ../dmptool/app/assets/stylesheets/vendor/dmptool-ui.css
-
-# Copy all javascripts over to the DMPTool assets directory
-cp dist/ui-assets/*.js ../dmptool/app/javascript/vendor/dmptoolUi.js
