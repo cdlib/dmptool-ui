5b8f19b5683e7eef04416129c90ad524d15a767c
briri <brian.riley@ucop.edu>
2021-11-23 07:31:42 -0800

    updated shell script and copied in just the _html.scss file from the dmptool app

--- copy_css_from_dmptool.sh
+++ copy_css_from_dmptool.sh
@@ -1,11 +1,2 @@
-# Move into the dmptool repo and run Bundler to install dependencies
-echo 'Installing dependencies ...'
-cd ../dmptool && bundle install
-
-# run the Rails asset compilation
-echo 'Compiling assets ...'
-bin/rails assets:clobber && bin/rails assets:precompile
-
-# Copy over the generate CSS file
-echo 'Copying CSS file ...'
-cp public/assets/application-*.css ../dmptool-ui/dmptool_html.css
+# Copy the DMPTool base HTML page layout styles to this repo
+cp ../dmptool/app/assets/stylesheets/blocks/_html.scss ./dmptool_html.css
