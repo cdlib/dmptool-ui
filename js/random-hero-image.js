876643f43d87c1d471465d1c644cd208264a40de
JoelCDL <joel.hagedorn@ucop.edu>
2022-03-04 14:01:42 -0800

    import hero images without using parcel url pipeline

--- js/random-hero-image.js
+++ js/random-hero-image.js
@@ -1,14 +1,14 @@
 // Random Hero Image //
 
-// Transform format, quality and size of original images via Parcel (see: https://v2.parceljs.org/recipes/image/):
+// Transform format, quality and size of original images using query parameters via Parcel (see: https://parceljs.org/recipes/image/):
 
-import heroImg1 from 'url:../images/hero/1-large.jpg?as=jpg&quality=30&width=1000'
-import heroImg2 from 'url:../images/hero/2-large.jpg?as=jpg&quality=30&width=1000'
-import heroImg3 from 'url:../images/hero/3-large.jpg?as=jpg&quality=30&width=1000'
-import heroImg4 from 'url:../images/hero/4-large.jpg?as=jpg&quality=30&width=1000'
-import heroImg5 from 'url:../images/hero/5-large.jpg?as=jpg&quality=30&width=1000'
-import heroImg6 from 'url:../images/hero/6-large.jpg?as=jpg&quality=30&width=1000'
-import heroImg7 from 'url:../images/hero/7-large.jpg?as=jpg&quality=30&width=1000'
+import heroImg1 from '../images/hero/1-large.jpg?as=jpg&quality=30&width=1000'
+import heroImg2 from '../images/hero/2-large.jpg?as=jpg&quality=30&width=1000'
+import heroImg3 from '../images/hero/3-large.jpg?as=jpg&quality=30&width=1000'
+import heroImg4 from '../images/hero/4-large.jpg?as=jpg&quality=30&width=1000'
+import heroImg5 from '../images/hero/5-large.jpg?as=jpg&quality=30&width=1000'
+import heroImg6 from '../images/hero/6-large.jpg?as=jpg&quality=30&width=1000'
+import heroImg7 from '../images/hero/7-large.jpg?as=jpg&quality=30&width=1000'
 
 const initRandomHeroImage = () => {
   if (document.querySelector('.c-calltoaction')) {
